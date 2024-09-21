078ae2b62d2cffee55d0bd88486ae21a08d01553
Vladislav <vladislav.seleznev99@gmail.com>
2024-09-22 02:58:17 +0300

    feat: Add url for user and account data managing

--- To_Do_List/main/urls.py
+++ To_Do_List/main/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import TaskViewSet, CategoryViewSet, PriorityViewSet
+from .views import TaskViewSet, CategoryViewSet, PriorityViewSet, UserViewSet
 
 router = routers.DefaultRouter()
+router.register('users', UserViewSet)
 router.register('tasks', TaskViewSet)
 router.register('categories', CategoryViewSet)
 router.register('priorities', PriorityViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
--- To_Do_List/main/views.py
+++ To_Do_List/main/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .serializers import TaskSerializer, CategorySerializer, PrioritySerializer
+from .serializers import TaskSerializer, CategorySerializer, PrioritySerializer, UserSerializer
 from .models import Task, Category, Priority
 from django.utils import timezone
 
@@ -80,5 +81,31 @@ class PriorityViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         instance.deleted = True
         instance.deleted_at = timezone.now()
+        instance.save()
+        return Response(status=204)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    # queryset = User.objects.filter(is_active=True) // TODO: for not staff user
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_active = False
         instance.save()
         return Response(status=204)
